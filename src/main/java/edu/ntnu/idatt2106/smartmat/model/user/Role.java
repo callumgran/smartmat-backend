5fbad78f29d56b5be6031e73e56936cb7d9cf3ce
Callum Gran <callum.gran@gmail.com>
2023-04-17 15:45:23 +0200

    created user model

---
+++ src/main/java/edu/ntnu/idatt2106/smartmat/model/user/Role.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2106.smartmat.model.user;
+
+/**
+ * Enum representing the role of a user.
+ * @author Callum G.
+ * @version 1.0 - 17.4.2023
+ */
+public enum Role {
+  USER,
+  ADMIN,
+}
