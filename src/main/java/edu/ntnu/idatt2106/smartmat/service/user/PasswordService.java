24ae7fbd888128389356830fa01f7501ab5c91fb
Callum Gran <callum.gran@gmail.com>
2023-04-17 18:25:33 +0200

    added tests for password checker

--- src/main/java/edu/ntnu/idatt2106/smartmat/service/user/PasswordService.java
+++ src/main/java/edu/ntnu/idatt2106/smartmat/service/user/PasswordService.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2106.smartmat.service.user;
 
+import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -17,8 +18,13 @@ public class PasswordService {
    * Method to hash a password.
    * @param password the password to hash.
    * @return the hashed password.
+   * @throws NullPointerException if the password is null.
+   * @throws IllegalArgumentException if the password is empty.
    */
-  public static String hashPassword(String password) {
+  public static String hashPassword(@NonNull String password) {
+    if (password.isBlank()) {
+      throw new IllegalArgumentException("Password cannot be empty");
+    }
     String salt = BCrypt.gensalt(); // generate a random salt value
     return salt + ":" + BCrypt.hashpw(password, salt);
   }
@@ -28,8 +34,13 @@ public class PasswordService {
    * @param password the password to check.
    * @param hashedPassword the hashed password to check against.
    * @return true if the password is correct.
+   * @throws NullPointerException if the password or hashed password is null.
+   * @throws IllegalArgumentException if the password or hashed password is empty.
    */
-  public static boolean checkPassword(String password, String hashedPassword) {
+  public static boolean checkPassword(@NonNull String password, @NonNull String hashedPassword) {
+    if (password.isBlank() || hashedPassword.isBlank()) {
+      throw new IllegalArgumentException("Cannot have blank values.");
+    }
     LOGGER.info(password + " -> " + hashedPassword);
     String[] parts = hashedPassword.split(":");
     String salt = parts[0];
