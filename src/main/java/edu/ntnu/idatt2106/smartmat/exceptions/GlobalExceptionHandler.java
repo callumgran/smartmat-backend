ede9b5453d80df591d4d00710b959ccc83043008
Callum Gran <callum.gran@gmail.com>
2023-04-18 09:59:02 +0200

    added global exception handler

---
+++ src/main/java/edu/ntnu/idatt2106/smartmat/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,197 @@
+package edu.ntnu.idatt2106.smartmat.exceptions;
+
+import edu.ntnu.idatt2106.smartmat.exceptions.user.EmailAlreadyExistsException;
+import edu.ntnu.idatt2106.smartmat.exceptions.user.UserDoesNotExistsException;
+import edu.ntnu.idatt2106.smartmat.exceptions.user.UsernameAlreadyExistsException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import org.hibernate.ObjectNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * A global exception handler which converts
+ * unhandled exceptions to a response object
+ * Logs the exception message to the console
+ * Based on the global exception handler from the IDATT2105 project.
+ * @author Carl. G, Nicolai H. B.
+ * @version 1.0 - 18.04.2023
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  /**
+   * Overrides the default handler for creating the response entity
+   * @param ex Exception - an exception thrown that needs to be handled. Can be a subclass of Exception
+   * @param body Object - the body of the response
+   * @param headers HttpHeaders - the headers of the response
+   * @param statusCode HttpStatusCode - the http status code of the response
+   * @param request WebRequest - the request that caused the exception
+   * @return ResponseEntity - a response entity with the exception message
+   */
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(
+    Exception ex,
+    Object body,
+    HttpHeaders headers,
+    HttpStatusCode statusCode,
+    WebRequest request
+  ) {
+    ExceptionResponse response = new ExceptionResponse(ex.getClass().getSimpleName());
+    return super.handleExceptionInternal(ex, response, headers, statusCode, request);
+  }
+
+  /**
+   * Creates a response entity as an exception response
+   * The body will be a json object with the exception message in `detail`
+   * @param ex Exception - The exception that was thrown. Can be a subclass of Exception
+   * @param status HttpStatus - The status code of the response
+   * @return ResponseEntity - A response entity with the exception message
+   */
+  private ResponseEntity<ExceptionResponse> getResponseEntityWithExceptionResponse(
+    Exception ex,
+    HttpStatus status
+  ) {
+    LOGGER.error("{}: {}", ex.getClass().getSimpleName(), ex.getMessage());
+    ExceptionResponse response = new ExceptionResponse(ex.getClass().getSimpleName());
+    return new ResponseEntity<>(response, new HttpHeaders(), status);
+  }
+
+  /**
+   * Handles exceptions that is from conflicts with the database
+   * Returns a 409 conflict response with the message that the exception contains
+   *
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(
+    value = {
+      IllegalStateException.class,
+      EmailAlreadyExistsException.class,
+      UsernameAlreadyExistsException.class,
+    }
+  )
+  public ResponseEntity<ExceptionResponse> handleConflict(Exception ex, WebRequest request) {
+    return getResponseEntityWithExceptionResponse(ex, HttpStatus.CONFLICT);
+  }
+
+  /**
+   * Handles exceptions from when trying to find a user that does not found
+   * Returns a 404 not found response with the message that the exception contains
+   *
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(
+    value = {
+      UserDoesNotExistsException.class, ObjectNotFoundException.class, MalformedURLException.class,
+    }
+  )
+  public ResponseEntity<ExceptionResponse> handleSpecificObjectDoesNotExist(
+    Exception ex,
+    WebRequest request
+  ) {
+    return getResponseEntityWithExceptionResponse(ex, HttpStatus.NOT_FOUND);
+  }
+
+  /**
+   * Handles exceptions from when logging in with wrong credentials
+   * Returns a 401 not authorized response with the message that the exception contains
+   *
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(value = { BadCredentialsException.class })
+  public ResponseEntity<ExceptionResponse> handleBadCredentialsException(
+    Exception ex,
+    WebRequest request
+  ) {
+    return getResponseEntityWithExceptionResponse(ex, HttpStatus.UNAUTHORIZED);
+  }
+
+  /**
+   * Handles exceptions that is from conflicts with the database or a file handler
+   * Returns a 500 internal server error response with the message
+   * that the exception contains
+   *
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(value = { DatabaseException.class, IOException.class })
+  public ResponseEntity<ExceptionResponse> handleDatabaseException(
+    Exception ex,
+    WebRequest request
+  ) {
+    return getResponseEntityWithExceptionResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  /**
+   * Handles exceptions where a null object
+   * was tried to be accessed, when an argument passed was not valid
+   * or when bad request was made
+   * Returns a 400 bad request response with custom message
+   *
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(value = { NullPointerException.class, IllegalArgumentException.class })
+  public ResponseEntity<ExceptionResponse> handleNullPointerException(
+    Exception ex,
+    WebRequest request
+  ) {
+    return getResponseEntityWithExceptionResponse(ex, HttpStatus.BAD_REQUEST);
+  }
+
+  /**
+   * Handles exceptions where a user is not authorized to perform an action
+   * Returns a 403 forbidden response with custom message
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(value = { AccessDeniedException.class })
+  public ResponseEntity<ExceptionResponse> handlePermissionDeniedException(
+    Exception ex,
+    WebRequest request
+  ) {
+    return getResponseEntityWithExceptionResponse(ex, HttpStatus.FORBIDDEN);
+  }
+
+  /**
+   * Handles general exceptions which have not been handled yet
+   * Returns a 500 internal server error response with a custom message
+   *
+   * @param ex The exception that was thrown
+   * @param request The request that caused the exception
+   * @return A response entity with the exception message
+   */
+  @ExceptionHandler(value = { Exception.class })
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ResponseEntity<ExceptionResponse> handleRemainingExceptions(
+    Exception ex,
+    WebRequest request
+  ) {
+    return getResponseEntityWithExceptionResponse(
+      new Exception(new Throwable(ex.getClass().getSimpleName() + ": " + ex.getMessage())),
+      HttpStatus.INTERNAL_SERVER_ERROR
+    );
+  }
+}
--- src/test/java/edu/ntnu/idatt2106/smartmat/endpoint/PublicUserControllerTest.java
+++ src/test/java/edu/ntnu/idatt2106/smartmat/endpoint/PublicUserControllerTest.java
@@ -1,155 +1,154 @@
-// package edu.ntnu.idatt2106.smartmat.endpoint;
+package edu.ntnu.idatt2106.smartmat.endpoint;
 
-// import static org.junit.Assert.fail;
-// import static org.mockito.Mockito.when;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// import edu.ntnu.idatt2106.smartmat.controller.PublicUserController;
-// import edu.ntnu.idatt2106.smartmat.exceptions.user.UserDoesNotExistsException;
-// import edu.ntnu.idatt2106.smartmat.exceptions.user.UsernameAlreadyExistsException;
-// import edu.ntnu.idatt2106.smartmat.model.user.Role;
-// import edu.ntnu.idatt2106.smartmat.model.user.User;
-// import edu.ntnu.idatt2106.smartmat.security.SecurityConfig;
-// import edu.ntnu.idatt2106.smartmat.service.user.UserService;
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-// import org.springframework.boot.test.mock.mockito.MockBean;
-// import org.springframework.http.MediaType;
-// import org.springframework.test.context.junit4.SpringRunner;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import edu.ntnu.idatt2106.smartmat.controller.PublicUserController;
+import edu.ntnu.idatt2106.smartmat.exceptions.user.UserDoesNotExistsException;
+import edu.ntnu.idatt2106.smartmat.exceptions.user.UsernameAlreadyExistsException;
+import edu.ntnu.idatt2106.smartmat.model.user.Role;
+import edu.ntnu.idatt2106.smartmat.model.user.User;
+import edu.ntnu.idatt2106.smartmat.security.SecurityConfig;
+import edu.ntnu.idatt2106.smartmat.service.user.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-// /**
-//  * Class used to test the public user endpoints.
-//  * Based on the PublicUserControllerTest class from the IDATT2105 project.
-//  */
-// @RunWith(SpringRunner.class)
-// @WebMvcTest({ PublicUserController.class, SecurityConfig.class })
-// public class PublicUserControllerTest {
+/**
+ * Class used to test the public user endpoints.
+ * Based on the PublicUserControllerTest class from the IDATT2105 project.
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest({ PublicUserController.class, SecurityConfig.class })
+public class PublicUserControllerTest {
 
-//   @Autowired
-//   private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-//   @MockBean
-//   private UserService userService;
+  @MockBean
+  private UserService userService;
 
-//   @Test
-//   public void testGetUserThatExists() throws Exception {
-//     User user = User
-//       .builder()
-//       .username("test")
-//       .password("test")
-//       .email("test@test.com")
-//       .role(Role.USER)
-//       .firstName("test")
-//       .lastName("test")
-//       .build();
+  @Test
+  public void testGetUserThatExists() throws Exception {
+    User user = User
+      .builder()
+      .username("test")
+      .password("test")
+      .email("test@test.com")
+      .role(Role.USER)
+      .firstName("test")
+      .lastName("test")
+      .build();
 
-//     when(userService.getUserByUsername("test")).thenReturn(user);
+    when(userService.getUserByUsername("test")).thenReturn(user);
 
-//     try {
-//       mvc
-//         .perform(
-//           MockMvcRequestBuilders.get("/api/v1/public/users/test").accept(MediaType.APPLICATION_JSON)
-//         )
-//         .andExpect(status().isOk());
-//     } catch (Exception e) {
-//       fail();
-//     }
-//   }
+    try {
+      mvc
+        .perform(
+          MockMvcRequestBuilders.get("/api/v1/public/users/test").accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk());
+    } catch (Exception e) {
+      fail();
+    }
+  }
 
-// Wait for global exception handler
-// @Test
-// public void testGetUserThatDoesNotExist() throws Exception {
-//   when(userService.getUserByUsername("test")).thenThrow(new UserDoesNotExistsException());
+  @Test
+  public void testGetUserThatDoesNotExist() throws Exception {
+    when(userService.getUserByUsername("test")).thenThrow(new UserDoesNotExistsException());
 
-//   try {
-//     mvc
-//       .perform(
-//         MockMvcRequestBuilders.get("/api/v1/public/users/test").accept(MediaType.APPLICATION_JSON)
-//       )
-//       .andExpect(status().isNotFound());
-//   } catch (Exception e) {
-//     fail();
-//   }
-// }
+    try {
+      mvc
+        .perform(
+          MockMvcRequestBuilders.get("/api/v1/public/users/test").accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isNotFound());
+    } catch (Exception e) {
+      fail();
+    }
+  }
 
-//   @Test
-//   public void testCreateUser() throws Exception {
-//     User user = User
-//       .builder()
-//       .username("test")
-//       .password("test")
-//       .email("test@test")
-//       .role(Role.USER)
-//       .firstName("test")
-//       .lastName("test")
-//       .build();
+  @Test
+  public void testCreateUser() throws Exception {
+    User user = User
+      .builder()
+      .username("test")
+      .password("test")
+      .email("test@test")
+      .role(Role.USER)
+      .firstName("test")
+      .lastName("test")
+      .build();
 
-//     when(userService.saveUser(user)).thenReturn(user);
+    when(userService.saveUser(user)).thenReturn(user);
 
-//     try {
-//       mvc
-//         .perform(
-//           MockMvcRequestBuilders
-//             .post("/api/v1/public/users")
-//             .accept(MediaType.APPLICATION_JSON)
-//             .contentType(MediaType.APPLICATION_JSON)
-//             .content(
-//               "{" +
-//               "  \"username\": \"test\"," +
-//               "  \"password\": \"Password1\"," +
-//               "  \"email\": \"test@test.com\"," +
-//               "  \"role\": \"USER\"," +
-//               "  \"firstName\": \"test\"," +
-//               "  \"lastName\": \"test\"," +
-//               "  \"listings\": []" +
-//               "}"
-//             )
-//         )
-//         .andExpect(status().isCreated());
-//     } catch (Exception e) {
-//       fail();
-//     }
-//   }
+    try {
+      mvc
+        .perform(
+          MockMvcRequestBuilders
+            .post("/api/v1/public/users")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(
+              "{" +
+              "  \"username\": \"test\"," +
+              "  \"password\": \"Password1\"," +
+              "  \"email\": \"test@test.com\"," +
+              "  \"role\": \"USER\"," +
+              "  \"firstName\": \"test\"," +
+              "  \"lastName\": \"test\"," +
+              "  \"listings\": []" +
+              "}"
+            )
+        )
+        .andExpect(status().isCreated());
+    } catch (Exception e) {
+      fail();
+    }
+  }
 
-//   @Test
-//   public void testCreateUserThatAlreadyExists() throws Exception {
-//     User user = User
-//       .builder()
-//       .username("test")
-//       .password("test")
-//       .email("test@test")
-//       .role(Role.USER)
-//       .firstName("test")
-//       .lastName("test")
-//       .build();
+  @Test
+  public void testCreateUserThatAlreadyExists() throws Exception {
+    User user = User
+      .builder()
+      .username("test")
+      .password("test")
+      .email("test@test")
+      .role(Role.USER)
+      .firstName("test")
+      .lastName("test")
+      .build();
 
-//     when(userService.saveUser(user)).thenThrow(new UsernameAlreadyExistsException());
+    when(userService.saveUser(user)).thenThrow(new UsernameAlreadyExistsException());
 
-//     try {
-//       mvc
-//         .perform(
-//           MockMvcRequestBuilders
-//             .post("/api/v1/public/users")
-//             .accept(MediaType.APPLICATION_JSON)
-//             .contentType(MediaType.APPLICATION_JSON)
-//             .content(
-//               "{" +
-//               "  \"username\": \"test\"," +
-//               "  \"password\": \"test\"," +
-//               "  \"email\": \"test@test.com\"," +
-//               "  \"role\": \"USER\"," +
-//               "  \"firstName\": \"test\"," +
-//               "  \"lastName\": \"test\"," +
-//               "}"
-//             )
-//         )
-//         .andExpect(status().isBadRequest());
-//     } catch (Exception e) {
-//       fail();
-//     }
-//   }
-// }
+    try {
+      mvc
+        .perform(
+          MockMvcRequestBuilders
+            .post("/api/v1/public/users")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(
+              "{" +
+              "  \"username\": \"test\"," +
+              "  \"password\": \"test\"," +
+              "  \"email\": \"test@test.com\"," +
+              "  \"role\": \"USER\"," +
+              "  \"firstName\": \"test\"," +
+              "  \"lastName\": \"test\"," +
+              "}"
+            )
+        )
+        .andExpect(status().isBadRequest());
+    } catch (Exception e) {
+      fail();
+    }
+  }
+}
