e19349d207bdb1fddd4be15c82fd13954716a91a
Callum Gran <callumg@stud.ntnu.no>
2023-04-21 09:46:14 +0200

    fix(unit-tests): added unit tests

--- src/test/java/edu/ntnu/idatt2106/smartmat/unit/household/HouseholdTest.java
+++ src/test/java/edu/ntnu/idatt2106/smartmat/unit/household/HouseholdTest.java
@@ -9,7 +9,6 @@ import edu.ntnu.idatt2106.smartmat.model.household.Household;
 import edu.ntnu.idatt2106.smartmat.model.household.HouseholdMember;
 import java.util.HashSet;
 import java.util.UUID;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -17,66 +16,56 @@ import org.junit.jupiter.api.Test;
  */
 public class HouseholdTest {
 
-  @Nested
-  public class HouseholdConstructorTest {
+  Household household;
 
-    Household household;
+  @Test
+  public void testHouseholdConstructor() {
+    String householdName = "householdName";
+    household = new Household(householdName);
 
-    @Test
-    public void testHouseholdConstructor() {
-      String householdName = "householdName";
-      household = new Household(householdName);
+    assertEquals(householdName, household.getName());
+  }
 
-      assertEquals(householdName, household.getName());
-    }
-
-    @Test
-    public void testHouseholdConstructorWithNulls() {
-      try {
-        household = new Household(null, null, null);
-        fail();
-      } catch (NullPointerException e) {
-        assertEquals(NullPointerException.class, e.getClass());
-      }
+  @Test
+  public void testHouseholdConstructorWithNulls() {
+    try {
+      household = new Household(null, null, null);
+      fail();
+    } catch (NullPointerException e) {
+      assertEquals(NullPointerException.class, e.getClass());
     }
   }
 
-  @Nested
-  public class HouseholdSettersTest {
+  @Test
+  public void testHouseholdSetters() {
+    household = new Household();
+    final UUID tmp = UUID.randomUUID();
+    final String householdName = "householdName";
+    final HashSet<HouseholdMember> members = new HashSet<HouseholdMember>();
+    household.setId(tmp);
+    household.setName(householdName);
+    household.setMembers(members);
 
-    Household household;
+    assertEquals(tmp, household.getId());
+    assertEquals(householdName, household.getName());
+    assertEquals(members, household.getMembers());
+  }
 
-    @Test
-    public void testHouseholdSetters() {
-      household = new Household();
-      final UUID tmp = UUID.randomUUID();
-      final String householdName = "householdName";
-      final HashSet<HouseholdMember> members = new HashSet<HouseholdMember>();
-      household.setId(tmp);
-      household.setName(householdName);
-      household.setMembers(members);
+  @Test
+  public void testHouseholdSetNameWithNull() {
+    household = new Household();
+    assertThrows(NullPointerException.class, () -> household.setName(null));
+  }
 
-      assertEquals(tmp, household.getId());
-      assertEquals(householdName, household.getName());
-      assertEquals(members, household.getMembers());
-    }
+  @Test
+  public void testHouseholdSetIdWithNull() {
+    household = new Household();
+    assertDoesNotThrow(() -> household.setId(null));
+  }
 
-    @Test
-    public void testHouseholdSetNameWithNull() {
-      household = new Household();
-      assertThrows(NullPointerException.class, () -> household.setName(null));
-    }
-
-    @Test
-    public void testHouseholdSetIdWithNull() {
-      household = new Household();
-      assertDoesNotThrow(() -> household.setId(null));
-    }
-
-    @Test
-    public void testHouseholdSetMembersWithNull() {
-      household = new Household();
-      assertDoesNotThrow(() -> household.setMembers(null));
-    }
+  @Test
+  public void testHouseholdSetMembersWithNull() {
+    household = new Household();
+    assertDoesNotThrow(() -> household.setMembers(null));
   }
 }
--- src/test/java/edu/ntnu/idatt2106/smartmat/unit/mapper/UserMapperTest.java
+++ src/test/java/edu/ntnu/idatt2106/smartmat/unit/mapper/UserMapperTest.java
@@ -7,25 +7,20 @@ import edu.ntnu.idatt2106.smartmat.dto.user.UserDTO;
 import edu.ntnu.idatt2106.smartmat.helperfunctions.TestUserEnum;
 import edu.ntnu.idatt2106.smartmat.mapper.user.UserMapper;
 import edu.ntnu.idatt2106.smartmat.model.user.User;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class UserMapperTest {
 
-  @Nested
-  public class UserToUserDTOTest {
+  @Test
+  public void testUserToUserDTO() {
+    User user = testUserFactory(TestUserEnum.GOOD);
 
-    @Test
-    public void testUserToUserDTO() {
-      User user = testUserFactory(TestUserEnum.GOOD);
+    UserDTO userDTO = UserMapper.INSTANCE.userToUserDTO(user);
 
-      UserDTO userDTO = UserMapper.INSTANCE.userToUserDTO(user);
-
-      assertEquals(user.getUsername(), userDTO.getUsername());
-      assertEquals(user.getEmail(), userDTO.getEmail());
-      assertEquals(user.getFirstName(), userDTO.getFirstName());
-      assertEquals(user.getLastName(), userDTO.getLastName());
-      assertEquals(user.getRole(), userDTO.getRole());
-    }
+    assertEquals(user.getUsername(), userDTO.getUsername());
+    assertEquals(user.getEmail(), userDTO.getEmail());
+    assertEquals(user.getFirstName(), userDTO.getFirstName());
+    assertEquals(user.getLastName(), userDTO.getLastName());
+    assertEquals(user.getRole(), userDTO.getRole());
   }
 }
